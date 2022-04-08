34ae74e1597481bc6b9bc5fd711caf5f6b5aee24
Petr Markeloff <macka.ukr@gmail.com>
2022-04-08 10:38:45 +0300

    renew config

--- compute.tf
+++ compute.tf
@@ -1,10 +1,10 @@
-resource "google_compute_instance" "test_instance" {
-   name = "test-instance"
+resource "google_compute_instance" "test_golang_instance" {
+   name = "test-golang-instance"
    machine_type = "e2-micro" # gcloud compute machine-types list
    zone = "${var.region}"
    boot_disk {
       initialize_params {
-         image = "debian-cloud/debian-9" # gcloud compute images list
+         image = "debian-cloud/debian-10" # gcloud compute images list
       }
    }
    network_interface {
